becda9c9950abc48530ebcac5a1771dbcfcdff77
Brendarg2004 <126797781+Brendarg2004@users.noreply.github.com>
2023-03-08 13:51:00 -0300

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function() {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "hobbie:natacion, segundo idioma: ingles (nivel intermedio), disponibilidad: full time, posibilidad de viajar: poseo licencia de conducir";
+}
